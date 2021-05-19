671c1578bfad3a60548466e0225ab2fe0855f900
Mariusz <mab152@aber.ac.uk>
2021-05-19 22:01:27 +0100

    Scraping but not perfectly

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,35 @@ require 'httparty'
 require 'byebug'
 
 def scraper
+    url= "https://www.totaljobs.com/jobs/in-london?radius=0"
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    job_listings = parsed_page.css('div.job.new')
+    job_listings.each do |job_listing|
+        job = {
+            title: job_listing.css('div.job-title').text.strip(),
+            company: job_listing.css('h3').text.strip(),
+            location: job_listing.css('li.location').text.strip(),
+            url: job_listing.css('a')[0].attributes["href"].value
+        }
+        byebug
+    end
 
 end
+
+scraper
+
+
+# div job.new
+# div.job-title
+# li salary
+
+
+#From here we can scrap stuff
+
+# jobCard = parsed_page.css('div.job.new')
+# jobCard.first.text
+# firstJob = jobCard.first
+# firstJob.css('div.job-title').text
+# firstJob.css('h3').text
+# firstJob.css('a')[0].attributes["href"].value  <-  scraping further looking for first value of href in those a
