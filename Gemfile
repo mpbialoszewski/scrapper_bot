c327f742ef0196d21c806347b3e20ee059af9eaa
Mariusz <mab152@aber.ac.uk>
2021-05-26 10:06:48 +0100

    Working CSV, adding comments to repositry showing understanding of code

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 gem "httparty"
 gem "nokogiri"
 gem "byebug"
+gem 'awesome_print'
+gem 'csv'
--- scraper.rb
+++ scraper.rb
@@ -1,59 +1,72 @@
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
+require 'awesome_print'
+require "addressable/uri"
+require "csv"
 
 def scraper
-    ##Scraping all available jobs now in London
-    url= "https://www.totaljobs.com/jobs/in-london?radius=0"
+    # using user-agent to check connection available 
+    page = `curl --user-agent "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)" https://london.craigslist.org `
+    url= "https://london.craigslist.org/search/hhh"
+    # Verifying if the URL is correct 
+    Addressable::URI.parse(url).host
+    # HttpParty initial use 
     unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    jobs = Array.new
-    job_listings = parsed_page.css('div.job.new')
-
-     # Divide all number of listing by number of pages and then rounding it up to
+    # Nokogiri parses as elements of arrays 
+    parsed_page = Nokogiri::HTML(unparsed_page.body)
+    # Creating new Array where we will store all the information about flat listings 
+    flats = Array.new
+    # Getting div with overview of all the information required 
+    flat_listings = parsed_page.css('div.result-info')
     page = 1
-    per_page = job_listings.count #currently 20
-    total = parsed_page.css('div.page-title').text.split(' ')[0].gsub(',','').to_i#38374 for London jobs currently available
-    last_page = (total.to_f / per_page.to_f).round
+    per_page = flat_listings.count #currently 120
+    total = parsed_page.css('span.totalcount')[0].text #total amount of listings on this page
+    # For now we are doing it only on one page as craigslist has quite weird pagination 
+    last_page = 1
     while page <= last_page
-        pagination_url = "https://www.totaljobs.com/jobs/in-london?radius=0&page=#{page}"
+        pagination_url = "https://london.craigslist.org/d/housing/search/hhh"
         puts pagination_url
-        puts "Page: #{page}"
+        puts "LISTING on page: #{page}"
         puts ''
 
         pagination_unparsed_page = HTTParty.get(pagination_url)
-        pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
-        pagination_job_listings = pagination_parsed_page.css('div.job.new')
-        pagination_job_listings.each do |job_listing|
-            job = {
-                title: job_listing.css('div.job-title').text.strip(),
-                company: job_listing.css('h3').text.strip(),
-                location: job_listing.css('li.location').text.strip(),
-                url: job_listing.css('a')[0].attributes["href"].value
+        pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page.body)
+        pagination_flat_listings = pagination_parsed_page.css('li.result-row')
+        pagination_flat_listings.each do |flat_listing|
+            
+            flat = {
+                title: flat_listing.css('h3')[0].text.strip(),
+                url: flat_listing.css('a')[0].attributes["href"].text,
+                # Price is a span that repeats twice in two places within the listing
+                # price: parsed_page.css('span.result-price')[0].text
             }
-            jobs << job
-            puts "Added #{job[:title]}"
-            puts ""
+            
+            flats << flat
+            
+            puts "Listing: #{flat[:title]}"
+            puts "Link: #{flat[:url]}" 
+            # puts "Price tag: #{flat[:price]}"
+
+            # Saving file to csv, a+ means it will append the data to the end of the document, 
+            # we can use 'w+' if we want to overwrite the document 
+
+            CSV.open("results.csv", "a+") do |csv|
+                csv << [flat[:title],flat [:url]]
+            end
+               
         end
         page += 1
     end
 end
 
+
 scraper
 
-## TODO: Read some more about gsub
 
 
-# div job.new
-# div.job-title
-# li salary
 
 
-#From here we can scrap stuff
 
-# jobCard = parsed_page.css('div.job.new')
-# jobCard.first.text
-# firstJob = jobCard.first
-# firstJob.css('div.job-title').text
-# firstJob.css('h3').text
-# firstJob.css('a')[0].attributes["href"].value  <-  scraping further looking for first value of href in those a
+
+
